9c88c59ccb410392e41fc3e82baa1d630f6ebe81
Sandro-Col <sandro.col@gmail.com>
2021-07-27 22:03:15 -0300

    updating our app with a shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,56 @@
 import React from 'react';
-
-import { HomePage } from './pages/homepage/homepage.componente';
+import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
+import HomePage from './pages/homepage/homepage.component.jsx';
+import ShopPage from './pages/shop/shop.component';
+
+const HatsPage = (props) => {
+  console.log(props)
+  return(
+    <div>
+      <h1>HATS PAGE </h1>
+    </div>
+  );
+};
+
+const JacketsPage = () => (
+  <div>
+    <h1>JACKETS PAGE</h1>
+  </div>
+);
+
+const SneakersPage = () => (
+  <div>
+    <h1>SNEAKERS PAGE</h1>
+  </div>
+);
+
+const WomensPage = () => (
+  <div>
+    <h1>WOMENS PAGE</h1>
+  </div>
+);
+
+const MensPage = () => (
+  <div>
+    <h1>MENS PAGE</h1>
+  </div>
+);
+
 function App() {
   return (
-    <div className="App">
-      <HomePage/>
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+        <Route path='/hats' component={HatsPage} />
+        <Route path='/jackets' component={JacketsPage} />
+        <Route path='/sneakers' component={SneakersPage} />
+        <Route path='/womens' component={WomensPage} />
+        <Route path='/mens' component={MensPage} />
+      </Switch>
     </div>
   );
 }
