f1a1a9169d4f360ca05677b9bee1786915c09a43
Sandro-Col <sandro.col@gmail.com>
2021-08-11 19:59:57 -0300

    added CartIcon and CartDropdown components to our application, also implemented cart redicer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,12 +3,15 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils.js';
+import CartIcon from '../cart-icon/cart-icon.component.jsx';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component.jsx';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
 
-const Header = ({ currentUser }) => (
+
+const Header = ({ currentUser, hidden }) => (
   <div className='header'>
     <Link className='logo-container' to="/">
       <Logo className='logo' />  
@@ -21,21 +24,31 @@ const Header = ({ currentUser }) => (
         CONTACT
       </Link>
       {
-        currentUser ?
-        <div className='option' onClick={() => auth.signOut()}>
-          SIGN OUT <span>( {currentUser.displayName} )</span>
-        </div>
-        :
-        <Link className='option' to='/signin'>
-          SIGN IN
-        </Link>
+        currentUser ? 
+        (
+          <div className='option' onClick={() => auth.signOut()}>
+            SIGN OUT <span>( {currentUser.displayName} )</span>
+          </div>
+        ) : (
+          <Link className='option' to='/signin'>
+            SIGN IN
+          </Link>
+        )
       }
+      <CartIcon />
     </div>
+    {
+      hidden ? 
+      null
+      : 
+      <CartDropdown />
+    }
   </div>
 );
 
 const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+  currentUser: state.user.currentUser,
+  hidden: state.cart.hidden
 })
 
 export default connect(mapStateToProps)(Header);
