6445d2f0070c1974fe7bc4db1df92437e8b5be65
Sandro-Col <sandro.col@gmail.com>
2021-09-06 20:21:02 -0300

    finished 188 (Redux Thunk)

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,13 +1,4 @@
 import { createSelector } from 'reselect';
-import memoize from 'lodash.memoize';
-
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5
-}
 
 const selectShop = state => state.shop;
 
@@ -18,12 +9,22 @@ export const selectCollections = createSelector(
 
 export const selectCollectionsForPreview = createSelector(
   [selectCollections],
-  collections => collections ? Object.keys(collections).map(key => collections[key]) : []
+  collections =>
+    collections ? Object.keys(collections).map(key => collections[key]) : []
 );
 
-export const selectCollection = memoize((collectionUrlParam) =>
+export const selectCollection = collectionUrlParam =>
   createSelector(
     [selectCollections],
     collections => (collections ? collections[collectionUrlParam] : null)
-  )
+  );
+
+export const selectIsCollectionFetching = createSelector(
+  [selectShop],
+  shop => shop.isFetching
+);
+
+export const selectIsCollectionsLoaded = createSelector(
+  [selectShop],
+  shop => !!shop.collections
 );
